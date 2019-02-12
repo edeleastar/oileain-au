ed3de5b4ce176ee8eb3e6ae3b00051b1104237ec
Eamonn de Leastar <edeleastar@wit.ie>
2019-02-12 12:54:55 +0000

    refactor header + some layout

--- webpack.config-deploy.js
+++ webpack.config-deploy.js
@@ -18,8 +18,8 @@ const title = 'Aurelia Navigation Skeleton';
 const outDir = path.resolve(__dirname, project.platform.output);
 const srcDir = path.resolve(__dirname, 'src');
 const nodeModulesDir = path.resolve(__dirname, 'node_modules');
-//const baseUrl = '/oileain-au-public';
-const baseUrl = '/';
+const baseUrl = '/oileain-au-public';
+//const baseUrl = '/';
 
 const cssRules = [
   { loader: 'css-loader' },
